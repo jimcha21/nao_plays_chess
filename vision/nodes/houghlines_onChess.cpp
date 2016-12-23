fc4919debc049f5f23d789bf5ac7db4faae0127b
jimcha21 <dchatziparaschis@gmail.com>
2016-12-23 21:47:38 +0200

    hough node minor changes (in role)

--- vision/nodes/houghlines_onChess.cpp
+++ vision/nodes/houghlines_onChess.cpp
@@ -30,20 +30,21 @@ static const std::string OPENCV_WINDOW = "Image window";
 Mat src, src_gray, dst;
 bool debug_mode=false;
 
-std::vector<cv::Point> chess_knob_vector_;
+std::vector<vision::ChessPoint> chess_knob_vector_;
 std::vector<cv::Point3d> temp_vector;
 
-int hough_thres=70;
+int hough_thres=20;
 
 //******************************************************************************************
 
 void chessboardVectorTopic(const vision::ChessVector& data){
-  cv::Point pt;
+  vision::ChessPoint pt;
   chess_knob_vector_.clear();
   //ROS_INFO("size %d",data.p_vector.size());
   for(int i=0;i<data.p_vector.size();i++){ //till 81..
     pt.x=data.p_vector[i].x;
     pt.y=data.p_vector[i].y;
+    pt.state=data.p_vector[i].state;
     chess_knob_vector_.push_back(pt);
   }
 }
@@ -120,17 +121,38 @@ public:
       /// Set some parameters
      
       vector<Vec4i> lines;
-      HoughLinesP(dst, lines, 1, CV_PI / 180, hough_thres,50,40);
-      ROS_INFO("%d lines found",lines.size());
+      HoughLinesP(dst, lines, 1, CV_PI / 180, hough_thres,(int)sqrt(pow((chess_knob_vector_[0].x-chess_knob_vector_[72].x),2)+pow((chess_knob_vector_[0].y-chess_knob_vector_[72].y),2))-80,40);
+      //ROS_INFO("%d lines found",lines.size());
       //cv::imshow("m to canny",dst);
 
       //ROS_INFO("O syntelesths iytthnshs einai %d",chess_knob_vector_[0].y);
 
+
+      float chess_vertical_slopes[9]={};
+      int column=0;
+      for(int j=0;j<9;j++){        
+        int start=0+column,end=8+column;
+        for(int p=0+column;p<9+column;p++){
+          ROS_INFO("p=%d",p);
+          if(chess_knob_vector_[p].state.compare("feat_mapped")==0||chess_knob_vector_[p].state.compare("shi_mapped")==0) ROS_INFO("BRIKE STHN %d grammh to %d hehe",linee,p);
+        }
+        line( src, Point(chess_knob_vector_[start].x,chess_knob_vector_[start].y), Point(chess_knob_vector_[end].x,chess_knob_vector_[end].y),Scalar( 255,0,0 ), 3, LINE_AA); 
+        ROS_INFO("termatise ");
+       // ROS_INFO("line me %d",line);
+        column=column+9;
+      }
+
        float la;
        float lambda_down=(float)(chess_knob_vector_[72].y-chess_knob_vector_[0].y)/(chess_knob_vector_[72].x-chess_knob_vector_[0].x);
+       float opa=(float)(chess_knob_vector_[73].y-chess_knob_vector_[1].y)/(chess_knob_vector_[73].x-chess_knob_vector_[1].x);
+        float opa1=(float)(chess_knob_vector_[74].y-chess_knob_vector_[2].y)/(chess_knob_vector_[74].x-chess_knob_vector_[2].x);
+         float opa2=(float)(chess_knob_vector_[75].y-chess_knob_vector_[3].y)/(chess_knob_vector_[75].x-chess_knob_vector_[3].x);
+       //ROS_INFO("O SYNTELESTHS EINAI %f %f %f %f",lambda_down,opa,opa1,opa2);
        float lambda_side=(float)(chess_knob_vector_[76].y-chess_knob_vector_[72].y)/(chess_knob_vector_[76].x-chess_knob_vector_[72].x);
-       ROS_INFO("O syntelesths iytthnshs einai %f sid:%f",lambda_down,lambda_side);
+       //ROS_INFO("O syntelesths iytthnshs einai %f sid:%f",lambda_down,lambda_side);
        int num_lines=0;bool end_case=true;
+       //ROS_INFO("Ara to distance apo akrhs s akrh einai %f",(float)sqrt(pow((chess_knob_vector_[0].x-chess_knob_vector_[72].x),2)+pow((chess_knob_vector_[0].y-chess_knob_vector_[72].y),2)));
+       //ROS_INFO("kai to orizontio %f",(float)sqrt(pow((chess_knob_vector_[80].x-chess_knob_vector_[72].x),2)+pow((chess_knob_vector_[80].y-chess_knob_vector_[72].y),2)));
       for( size_t i = 0; i < lines.size() && end_case; i++ )
       {
           Vec4i l = lines[i];
@@ -141,63 +163,91 @@ public:
           //if(l[1]>445 && l[3]>445) //chessboard spatial limiter for hough eytheies lysh
           la=(float)(l[3]-l[1])/(l[2]-l[0]);
           //line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( rng.uniform(0,255), rng.uniform(0,255), rng.uniform(0,255) ), 3, LINE_AA);
-          ROS_INFO("la is %f",la);
-          if(abs(la-lambda_down)<=1){
-             ROS_INFO("brike mia eytheia me la=%f",la);
+          line( src, Point(chess_knob_vector_[54].x,chess_knob_vector_[54].y), Point(chess_knob_vector_[55].x,chess_knob_vector_[55].y),Scalar( 255,0,0 ), 3, LINE_AA);   
+          //ROS_INFO("la is %f",la);
+
+          if(abs(la-lambda_down)<=5){
+            // ROS_INFO("line with coord %d %d %d %d",l[0],l[2],l[1],l[3]);
             num_lines++;
-            if(num_lines==15){
-              end_case=false;
+            if(num_lines==10){
+              end_case=true;//!
             }
 
             int x_mid=(int)(l[0]+l[2])/2;
             int y_mid=(int)(l[1]+l[3])/2;
 
-            float point_distance=9999;
+            float point_distance=9999,point_distance_a=9999,point_distance_b=9999;
             int nearest;
             for(int j=36;j<=44;j++){
-              float temp_dist=(float)sqrt(pow((x_mid-chess_knob_vector_[j].x),2)+pow((y_mid-chess_knob_vector_[j].y),2));
-              if(temp_dist<point_distance){                  
-                point_distance= temp_dist;
+              float temp_mid_dist=(float)sqrt(pow((x_mid-chess_knob_vector_[j].x),2)+pow((y_mid-chess_knob_vector_[j].y),2));
+              float temp_a_dist=(float)sqrt(pow((l[0]-chess_knob_vector_[j].x),2)+pow((l[1]-chess_knob_vector_[j].y),2));
+              float temp_b_dist=(float)sqrt(pow((l[2]-chess_knob_vector_[j].x),2)+pow((l[3]-chess_knob_vector_[j].y),2));
+              //ROS_INFO("Distances are %f %f %f",temp_mid_dist,temp_a_dist,temp_b_dist);
+              if(temp_mid_dist<point_distance/*&&temp_a_dist<point_distance_a&&temp_b_dist<point_distance_b*/){                  
+                //ROS_INFO("min found at %d",j);
+                point_distance= temp_mid_dist;
+                point_distance_b=temp_b_dist;
+                point_distance_a=temp_a_dist;
                 nearest=j;
               }
+
             }
-            if(nearest==36){
+            //ROS_INFO("nearest is %d",nearest);
+            /*if(nearest==36){
               //line( src, Point(l[0],l[1]), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+              line( src, Point(x_mid,y_mid), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+            
               line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( 0,0,255 ), 3, LINE_AA);            
             }else if(nearest==37){
               //line( src, Point(l[0],l[1]), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+              line( src, Point(x_mid,y_mid), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+            
               line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( 0,255,0 ), 3, LINE_AA);            
             }else if(nearest==38){
               //line( src, Point(l[0],l[1]), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+            line( src, Point(x_mid,y_mid), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+            
               line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( 255,0,0 ), 3, LINE_AA);            
             }else if(nearest==39){
               //line( src, Point(l[0],l[1]), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+              line( src, Point(x_mid,y_mid), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+            
               line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( 255,255,0 ), 3, LINE_AA);            
             }else if(nearest==40){
               //line( src, Point(l[0],l[1]), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+                            line( src, Point(x_mid,y_mid), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+            
               line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( 0,255,255 ), 3, LINE_AA);            
             }else if(nearest==41){
               //line( src, Point(l[0],l[1]), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+                            line( src, Point(x_mid,y_mid), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+            
               line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( 255,0,255 ), 3, LINE_AA);            
             }else if(nearest==42){
               //line( src, Point(l[0],l[1]), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
-              line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( 0,255,125 ), 3, LINE_AA);            
+                          line( src, Point(x_mid,y_mid), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+            
+              line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( 0,0,125 ), 3, LINE_AA);            
             }else if(nearest==43){
               //line( src, Point(l[0],l[1]), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
-              line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( 125,255,0 ), 3, LINE_AA);            
+                          line( src, Point(x_mid,y_mid), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+            
+              line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( 0,125,0 ), 3, LINE_AA);            
             }else if(nearest==44){
               //line( src, Point(l[0],l[1]), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
-              line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( 125,255,100 ), 3, LINE_AA);            
-            }
+                          line( src, Point(x_mid,y_mid), Point( chess_knob_vector_[nearest].x,chess_knob_vector_[nearest].y),Scalar(255,0,0), 3, LINE_AA); 
+            
+              line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( 125,0,0 ), 3, LINE_AA);            
+            }*/
 
-            line( src, Point(chess_knob_vector_[36].x,chess_knob_vector_[36].y), Point(chess_knob_vector_[37].x,chess_knob_vector_[37].y),Scalar( 255,0,0 ), 3, LINE_AA);   
+            //line( src, Point(chess_knob_vector_[36].x,chess_knob_vector_[36].y), Point(chess_knob_vector_[37].x,chess_knob_vector_[37].y),Scalar( 255,0,0 ), 3, LINE_AA);   
           }/*
           else if(abs(la-lambda_side)<=1){
             line( src, Point(l[0], l[1]), Point(l[2], l[3]),Scalar( rng.uniform(0,255), rng.uniform(0,255), rng.uniform(0,255) ), 3, LINE_AA);
           }*/
       }
 
-    ROS_INFO("%d filtered lines",num_lines);
+    //ROS_INFO("%d filtered lines",num_lines);
 }
       //line( src, Point(70, 691), Point(290,482), Scalar(0,0,255), 3, LINE_AA);
       //cv::imshow(OPENCV_WINDOW, cv_ptr->image);*/
@@ -206,7 +256,6 @@ public:
       //cv::imshow(OPENCV_WINDOW, cv_ptr->image);
       //imwrite( "easy_13.jpg", src ); //for storing the snapped images..
       cv::imshow(OPENCV_WINDOW,src);
-      //cv::imshow("OPENCV_WINDOW2",src_gray);
       cv::waitKey(3);
 
       // Output modified video stream
